876472198eefaf7532f8a94d5f75e0ebb60bd60c
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-14 16:18:29 -0700

    Update arc.js

--- path-calculator/arc.js
+++ path-calculator/arc.js
@@ -6,7 +6,7 @@ crossPlatformShapes.pathCalculator.arc = function(data){
     width = data.width,
     height = data.height;
 
-  var yControlPoint = ((height * (2/3))-(0.5*height));
+  var yControlPoint = (height * (2/3);
 
   var pathData = [{command: 'moveTo', points: [x, y-0.5*height]},
     {command: 'bezierCurveTo', points: [(x), (y-0.5*height + yControlPoint), (x + width), (y-0.5*height + yControlPoint), (x + width), (y-0.5*height)]}];
