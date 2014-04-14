fb5272e392524ef2a991ed4819596c3a9f3c0f2c
khanspers <khanspers@gladstone.ucsf.edu>
2014-04-14 16:17:17 -0700

    moves the arc up

--- path-calculator/arc.js
+++ path-calculator/arc.js
@@ -6,10 +6,10 @@ crossPlatformShapes.pathCalculator.arc = function(data){
     width = data.width,
     height = data.height;
 
-  var yControlPoint = height * (2/3);
+  var yControlPoint = ((height * (2/3))-(0.5*height));
 
-  var pathData = [{command: 'moveTo', points: [x, y]},
-    {command: 'bezierCurveTo', points: [(x), (y + yControlPoint), (x + width), (y + yControlPoint), (x + width), (y)]}];
+  var pathData = [{command: 'moveTo', points: [x, y-0.5*height]},
+    {command: 'bezierCurveTo', points: [(x), (y-0.5*height + yControlPoint), (x + width), (y-0.5*height + yControlPoint), (x + width), (y-0.5*height)]}];
 
   return pathData;
 };
